2140024d948cef21f132a8e1118fdf6c418515a2
Meenu Sabu <meenu.sabu@experionglobal.com>
2016-04-19 15:57:26 +0530

    new component creation

--- frontEnd/stock/stock-partial/stock-partial.js
+++ frontEnd/stock/stock-partial/stock-partial.js
@@ -1,15 +1,24 @@
-(function(){
+  (function(){
 
-function StockIndexController(Restangular){
-var ctrl=this;
-function index(){
-var baseStock=Restangular.all('stock');
-baseStock.getList().then(function(data) {
-ctrl.stockData = data;
-});
-}
+  function StockIndexController(Restangular,$state){
+  var ctrl=this;
+  ctrl.remove=remove;
 
-index();
-}
-angular.module('stock').controller('StockIndexController',StockIndexController);
-})();
+  function index(){
+  var baseStock=Restangular.all('stock');
+  baseStock.getList().then(function(data) {
+  ctrl.stockData = data;
+  });
+  }
+
+  function remove(item){
+    item.remove().then(function(data){
+      console.log(data);
+      $state.reload();
+    });
+  }
+
+  index();
+  }
+  angular.module('stock').controller('StockIndexController',StockIndexController);
+  })();
